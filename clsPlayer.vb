dde22decd5abd09162368e4a18f29db2c6957a05
James Flippin <45156789+JamesFlippin@users.noreply.github.com>
2020-11-17 10:22:13 -0600

    Added comments

--- clsPlayer.vb
+++ clsPlayer.vb
@@ -1,4 +1,25 @@
-﻿Public Class clsPlayer
+﻿'*******************************************
+'*******************************************
+'Programmer: James Flippin
+'Course: ITSE 1332.10x1 (VB)
+'Program purpose: This is the class file that
+'                 is the basis for the player
+'                 or the monster in this game
+'*******************************************
+'*******************************************
+
+#Region "Compiler_Directives"
+
+'*******************************************
+Option Explicit On
+Option Strict On
+Option Infer Off
+#Disable Warning IDE1006
+'*******************************************
+
+#End Region
+
+Public Class clsPlayer
 
     'Private Variables only viewable/accessible within the class
     'Typically private variables in classes are prefixes with an _ to denote private
@@ -9,16 +30,24 @@
     Private _intHealthPoints As Integer = 0
     Public _bolDead As Boolean = False
 
+    'A sub for later use with a "Start new Game" option to reset the values
     Public Sub Reset()
         _bolDead = False
+        _strName = String.Empty
+        _strTeam = String.Empty
+        _intDamageCanCause = 0
+        _intHealthPoints = 0
     End Sub
 
+    'Used to indicate the player/monster based on this class is alive or dead
     Public ReadOnly Property bolDead As Boolean
         Get
             Return _bolDead
         End Get
     End Property
 
+    'Holds the name of the player or the type of the Monster
+    'IE: Bob or Space Squid
     Public Property strName As String
         Set(strValue As String)
             _strName = strValue
@@ -28,6 +57,9 @@
         End Get
     End Property
 
+    'Holds the name of the Team the player belongs to,
+    'But for monsters, it is just left empty
+    'IE: Gardians of the Galaxy
     Public Property strTeam As String
         Set(strValue As String)
             _strTeam = strValue
@@ -37,6 +69,7 @@
         End Get
     End Property
 
+    'Sets or retrieves the amount of damage the player or monster can cause
     Public Property intDamageCanCause As Integer
         Set(intValue As Integer)
             _intDamageCanCause = intValue
@@ -47,6 +80,7 @@
         End Get
     End Property
 
+    'Sets or retrieves the amount of Help points the player or monster currently has
     Public Property intHeathPoints As Integer
         Set(intValue As Integer)
             _intHealthPoints = intValue
@@ -56,6 +90,7 @@
         End Get
     End Property
 
+    'Sets the amount of damage done by a successful attack
     Public Sub Attacked(ByVal intDamageDone As Integer)
         _intHealthPoints -= intDamageDone
         If _intHealthPoints <= 0 Then
